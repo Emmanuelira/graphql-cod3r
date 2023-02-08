1fa5f6ef2570094967641407a425a69130bcbe7f
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2023-02-08 18:10:06 -0300

    Criando um scalar do tipo Any para receber valores diferentes dos tipos básicos do GraphQL.

--- schema-query/index.js
+++ schema-query/index.js
@@ -1,14 +1,24 @@
 const { ApolloServer, gql } = require("apollo-server");
 const typeDefs = gql`
+    scalar Any
+
     # Pontos de entrada da sua API!
     type Query {
         ola: String
+        horaAtual : String
+        horaDate: Any
     }
 `;
 const resolvers = {
     Query: {
         ola() {
             return 'string qualquer';
+        }, 
+        horaAtual () {
+            return new Date().toString();
+        },
+        horaDate () {
+            return new Date();
         }
     }
 };
