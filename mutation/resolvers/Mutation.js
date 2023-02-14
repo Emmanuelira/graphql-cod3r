bc5a5a0554b108ad147a7325602d54f7612cc695
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2023-02-14 15:58:08 -0300

    Validando emails ao cadastrar um novo usuário.

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,16 +1,18 @@
 const { usuarios, proximoId } = require('../data/db');
 
 module.exports = {
-    novoUsuario(_, { nome, email, idade }) {
-        const usuario = {
-            id: proximoId(),
-            nome,
-            email,
-            idade,
-            perfil_id: 1,
-            status: "ATIVO"
-        };
-        usuarios.push(usuario);
-        return usuario;
+    novoUsuario(_, args) {
+        if(usuarios.some(u => u.email === args.email)) {
+            throw new Error('E-mail já cadastrado.');
+        } else {
+            const usuario = {
+                id: proximoId(),
+                ...args,
+                perfil_id: 1,
+                status: "ATIVO"
+            };
+            usuarios.push(usuario);
+            return usuario;
+        }
     }
 }
