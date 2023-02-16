66a064d83f786f3ef0865fc3a1d241c7fe3f4b2a
Emmanuelira <emmanuel.nobrega.lira@gmail.com>
2023-02-15 21:37:30 -0300

    Criando input para passagem de parâmetros na mutation de cadastrar um novo usuário.

--- mutation/resolvers/Mutation.js
+++ mutation/resolvers/Mutation.js
@@ -1,13 +1,13 @@
 const { usuarios, proximoId } = require('../data/db');
 
 module.exports = {
-    novoUsuario(_, args) {
-        if(usuarios.some(u => u.email === args.email)) {
+    novoUsuario(_, { dados }) {
+        if(usuarios.some(u => u.email === dados.email)) {
             throw new Error('E-mail já cadastrado.');
         } else {
             const usuario = {
                 id: proximoId(),
-                ...args,
+                ...dados,
                 perfil_id: 1,
                 status: "ATIVO"
             };
